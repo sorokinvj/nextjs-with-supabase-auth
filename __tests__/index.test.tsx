01072ac8bb2f3353cc5f1d5ec3491e40f6fefdb0
Vladislav Sorokin <sorokinvj@gmail.com>
2022-06-05 19:20:53 +0100

    update a test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -4,10 +4,15 @@ import starships from './starships.json'
 
 describe('Home', () => {
   test('it renders and contains starships', () => {
+    // arrange
     render(<Home starships={starships} error={null} />)
+
+    // act
     const page = screen.getByTestId('main page')
-    expect(page).toBeInTheDocument()
     const ships = screen.getAllByTestId('starship')
+
+    // assert
+    expect(page).toBeInTheDocument()
     expect(ships).toHaveLength(starships.results.length)
   })
 })
